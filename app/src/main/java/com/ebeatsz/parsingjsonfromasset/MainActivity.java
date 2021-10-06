45451643f21de67df746075be4b47f126aeca10d
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-10-06 19:28:41 +0100

    Xml code and json file added

--- app/src/main/java/com/ebeatsz/parsingjsonfromasset/MainActivity.java
+++ app/src/main/java/com/ebeatsz/parsingjsonfromasset/MainActivity.java
@@ -1,14 +1,20 @@
 package com.ebeatsz.parsingjsonfromasset;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 1 - Widgets
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
