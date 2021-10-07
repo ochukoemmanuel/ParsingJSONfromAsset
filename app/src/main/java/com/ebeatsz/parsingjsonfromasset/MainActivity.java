fd56c2c8d94dde68587e0d1dca1cfe486d2b7a9e
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-10-07 22:49:50 +0100

    load json from file

--- app/src/main/java/com/ebeatsz/parsingjsonfromasset/MainActivity.java
+++ app/src/main/java/com/ebeatsz/parsingjsonfromasset/MainActivity.java
@@ -1,14 +1,26 @@
 package com.ebeatsz.parsingjsonfromasset;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // 1 - Widgets
     RecyclerView recyclerView;
+
+    ArrayList<String> personName = new ArrayList<>();
+    ArrayList<String> emailIds = new ArrayList<>();
+    ArrayList<String> mobileNumbers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +28,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        try {
+            JSONObject obj = new JSONObject(loadJSONfromAssets());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private String loadJSONfromAssets() {
+        String json = null;
+
+        try {
+            InputStream is = getAssets().open("users_list.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
